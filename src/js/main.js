f44d81ea7827211dd1a7aa6330af14ff431de318
AndrusovaK <andrusova_ksenia@mail.ru>
2016-05-02 23:53:29 +0300

    active links

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+	var scrolling = false;
 
 	// Показ всех спикеров
 	$("#all-speakers").on('click', function (e) {
@@ -17,7 +18,7 @@ $(function(){
 
 	//Плавный скролл
 
-	$(".main-nav__link, .buy-ticket").click(function(e){
+	$(".main-nav__link, .buy-ticket, #book_button").click(function(e){
 		e.preventDefault();
 		$("html, body").animate({
 			scrollTop: $($(this).attr("href")).offset().top + "px"
@@ -25,7 +26,33 @@ $(function(){
 			scrolling = false;
 		});
 		return false;
-
 	});
+
+	//Активный класс у ссылок
+
+	$(window).scroll(function(){
+			$(".in_menu_block").each(function () {
+				var scrollTop = $(window).scrollTop(),
+					currentTop = $(this).offset().top,
+					thisId = $(this).attr('id'),
+					menuItem = $('.main-nav__item'),
+					thisHeight = $(this).outerHeight(),
+					currentBottom = currentTop + thisHeight,
+					thisLink = $("#" + thisId + "__link");
+
+				if (scrollTop >= currentTop - 50 && scrollTop < currentBottom){
+					console.log('adding active class');
+					menuItem.removeClass('main-nav__item--active');
+					thisLink.addClass("main-nav__item--active");
+				} else if (scrollTop < currentTop || scrollTop > currentBottom) {
+					console.log('removing active class');
+					thisLink.removeClass("main-nav__item--active");
+				}
+			});
+		});
+
+		//parallax
+
+	$(window).scroll();
 
 });
